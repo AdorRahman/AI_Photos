c6b8f3860afd662213cdaccbade2026827ca5267
AdorRahman <ador1234ador@gmaol.com>
2023-02-02 03:53:49 +0600

    new-all6

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -90,7 +90,8 @@ const CreatePost = () => {
           Create a Post
         </h1>
         <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
-          Generate unlimited images with Artificial Intelligence. Advanced
+          Generate unlimited images with{" "}
+          <span className="text-[#000]">Artificial Intelligence</span>. Advanced
           algorithms craft unique pictures. Unleash your creativity now, with
           your given <span className="text-[#000]">prompt</span>. <br />
           No refreshes allowed, share your creations with the community.
