86a39c38857422c5353eb5764c6ef51ea1873cfb
AdorRahman <ador1234ador@gmaol.com>
2023-02-04 11:00:37 +0600

    addall-1

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -84,90 +84,93 @@ const CreatePost = () => {
   };
 
   return (
-    <section className="max-w-7xl mx-auto">
-      <div>
-        <h1 className="font-extrabold text-[#222328] text-[32px]">
-          Create a Post
-        </h1>
-        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
-          Generate unlimited images with <span className="text-[#000]">AI</span>
-          . Advanced algorithms craft unique pictures. Unleash your creativity
-          now, with your given <span className="text-[#000]">prompt</span>.{" "}
-          <br />
-          No refreshes allowed, share your creations with the community.
-        </p>
-      </div>
-
-      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
-        <div className="flex flex-col gap-5">
-          <FormField
-            labelName="Your Name"
-            type="text"
-            name="name"
-            placeholder="Ex..."
-            value={form.name}
-            handleChange={handleChange}
-          />
-
-          <FormField
-            labelName="Prompt"
-            type="text"
-            name="prompt"
-            placeholder="Let your imagination run with prompt..."
-            value={form.prompt}
-            handleChange={handleChange}
-            isSurpriseMe
-            handleSurpriseMe={handleSurpriseMe}
-          />
-
-          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-80 p-3 h-80 flex justify-center items-center">
-            {form.photo ? (
-              <img
-                src={form.photo}
-                alt={form.prompt}
-                className="w-full h-full object-contain"
-              />
-            ) : (
-              <img
-                src={preview}
-                alt="preview"
-                className="w-9/12 h-9/12 object-contain opacity-40"
-              />
-            )}
-
-            {generatingImg && (
-              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
-                <Loader />
-              </div>
-            )}
-          </div>
-        </div>
-
-        <div className="mt-5 flex gap-5">
-          <button
-            type="button"
-            onClick={generateImage}
-            className=" text-white bg-[#000] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-          >
-            {generatingImg
-              ? "This should not take long (up to 2 minutes)..."
-              : "Generate"}
-          </button>
-        </div>
-
-        <div className="mt-10">
-          <p className="mt-2 text-[#666e75] text-[14px]">
-            ** Generate until you are happy and share your masterpieces with the
-            community using the button below. **
+    <section className="w-full flex mx-auto">
+      <main className="max-w-7xl mx-auto">
+        <div>
+          <h1 className="font-extrabold text-[#222328] text-[32px]">
+            Create a Post
+          </h1>
+          <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
+            Generate unlimited images with{" "}
+            <span className="text-[#000]">AI</span>. Advanced algorithms craft
+            unique pictures. Unleash your creativity now, with your given{" "}
+            <span className="text-[#000]">prompt</span>. <br />
+            No refreshes allowed, share your creations with the community.
           </p>
-          <button
-            type="submit"
-            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-          >
-            {loading ? "Sharing..." : "Share with the Community"}
-          </button>
         </div>
-      </form>
+
+        <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-5">
+            <FormField
+              labelName="Your Name"
+              type="text"
+              name="name"
+              placeholder="Ex..."
+              value={form.name}
+              handleChange={handleChange}
+            />
+
+            <FormField
+              labelName="Prompt"
+              type="text"
+              name="prompt"
+              placeholder="Let your imagination run with prompt..."
+              value={form.prompt}
+              handleChange={handleChange}
+              isSurpriseMe
+              handleSurpriseMe={handleSurpriseMe}
+            />
+
+            <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-80 p-3 h-80 flex justify-center items-center">
+              {form.photo ? (
+                <img
+                  src={form.photo}
+                  alt={form.prompt}
+                  className="w-full h-full object-contain"
+                />
+              ) : (
+                <img
+                  src={preview}
+                  alt="preview"
+                  className="w-9/12 h-9/12 object-contain opacity-40"
+                />
+              )}
+
+              {generatingImg && (
+                <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                  <Loader />
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div className="mt-5 flex gap-5">
+            <button
+              type="button"
+              onClick={generateImage}
+              className=" text-white bg-[#000] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            >
+              {generatingImg
+                ? "This should not take long (up to 2 minutes)..."
+                : "Generate"}
+            </button>
+          </div>
+
+          <div className="mt-10">
+            <p className="mt-2 text-[#666e75] text-[14px]">
+              ** Generate until you are happy and share your masterpieces with
+              the community using the button below. **
+            </p>
+            <button
+              type="submit"
+              className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            >
+              {loading ? "Sharing..." : "Share with the Community"}
+            </button>
+          </div>
+        </form>
+      </main>
+      <div className="w-[20vw]"></div>
     </section>
   );
 };
